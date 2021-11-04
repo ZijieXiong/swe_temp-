d0fe5de4a38f7be9d0c1a081e231a07ed2b17d83
Zijie Xiong <zx811@nyu.edu>
2021-11-03 21:58:05 -0400

    Add tests for food_menu endpoint.

--- API/tests/tests_endpoints.py
+++ API/tests/tests_endpoints.py
@@ -18,3 +18,30 @@ class EndpointTestCase(TestCase):
         ret = hello.get()
         self.assertIsInstance(ret, dict)
         self.assertIn(ep.HELLO, ret)
+
+    def test_food_menu1(self):
+        """
+        Post-condition 1: return is a dictionary
+        """
+        fm = ep.FoodMenu(Resource)
+        ret = fm.get()
+        self.assertIsInstance(ret, dict)
+    
+    def test_food_menu2(self):
+        """
+        Post-condition 2: keys to the dictionary are strings
+        """
+        fm = ep.FoodMenu(Resource)
+        ret = fm.get()
+        for key in ret:
+            self.assertIsInstance(key, str)
+    
+    def test_food_menu3(self):
+        """
+        Post-condition 3: values in the dict are dicts too.
+        """
+        fm = ep.FoodMenu(Resource)
+        ret = fm.get()
+        for val in ret.values():
+            self.assertIsInstance(val, dict)
+
