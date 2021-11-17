e91b9a22306699ec00cff7db8e6afa5192f00145
Zijie Xiong <zx811@nyu.edu>
2021-11-16 20:30:07 -0500

    added test for fetching food menu.

--- db/db.py
+++ db/db.py
@@ -19,23 +19,27 @@ FOOD_MENU_DB = f"{DB_DIR}/food_menu.json"
 ROOMS_DB = f"{DB_DIR}/rooms.json"
 
 
+def read_collection(perm_version):
+    """
+    A function to read a colleciton off of disk.
+    """
+    try:
+        with open(perm_version) as file:
+            return json.loads(file.read())
+    except FileNotFoundError:
+        print(f"{perm_version} not found.")
+        return None
+
+
 def get_rooms():
     """
     A function to return all chat rooms
     """
-    try:
-        with open(ROOMS_DB) as file:
-            return json.loads(file.read())
-    except FileNotFoundError:
-        return None
+    return read_collection(ROOMS_DB)
 
 
 def get_food_menu():
     """
     A function to return food menu stored in data base
     """
-    try:
-        with open(FOOD_MENU_DB) as file:
-            return json.loads(file.read())
-    except FileNotFoundError:
-        return None
+    return read_collection(FOOD_MENU_DB)
--- db/tests/test_data.py
+++ db/tests/test_data.py
@@ -6,3 +6,10 @@ This file holds the tests for db.py.
 from unittest import TestCase, skip
 
 import db.db as db
+
+def test_get_food_menu(self):
+        """
+        Can we fetch user db?
+        """
+        menu = db.get_food_menu()
+        self.assertIsInstance(menu, dict)
