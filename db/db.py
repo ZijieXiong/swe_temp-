505b74225ef51f0e29b577fa42323d4b68c028ec
Zijie Xiong <zx811@nyu.edu>
2021-12-10 20:35:15 -0500

    Connect to MongoDB

--- db/db.py
+++ db/db.py
@@ -3,21 +3,26 @@ This file will manage interactions with our data store.
 At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
-
 import json
 import os
+
+import db.db_connect as dbc
 
 SWE_HOME = os.environ["SWE_HOME"]
 TEST_MODE = os.environ.get("TEST_MODE", 0)
 
 if TEST_MODE:
-    DB_DIR = f"{SWE_HOME}/db/test_dbs"
+    # need to be switched to test db
+    DB_NAME = "sweDB"
 else:
-    DB_DIR = f"{SWE_HOME}/db"
+    DB_NAME = "sweDB"
 
+DB_DIR = f"{SWE_HOME}/db"
 FOOD_MENU_DB = f"{DB_DIR}/food_menu.json"
-DRINK_MENU_DB = f"{DB_DIR}/drink_menu.json"
 ROOMS_DB = f"{DB_DIR}/rooms.json"
+
+client = dbc.get_client()
+print(client)
 
 
 def read_collection(perm_version):
@@ -44,10 +49,3 @@ def get_food_menu():
     A function to return food menu stored in data base
     """
     return read_collection(FOOD_MENU_DB)
-
-
-def get_drink_menu():
-    """
-    A function to return food menu stored in data base
-    """
-    return read_collection(DRINK_MENU_DB)
